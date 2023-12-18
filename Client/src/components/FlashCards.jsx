c4e0f8c6b6f7a28f08afdadcda525984b0fa85d8
Gulaghaa <gulagaabdul123@gmail.com>
2023-12-18 23:52:34 +0400

    Display Error, CSS added

--- Client/src/components/FlashCards.jsx
+++ Client/src/components/FlashCards.jsx
@@ -1,83 +1,88 @@
-import React, { useEffect, useState } from "react";
-import { fetchJsonData, postJsonData } from "../services/getAPI";
-import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from 'react';
+import { fetchJsonData, postJsonData } from '../services/getAPI';
 import Modal from 'react-modal';
+import '../style/FlashCards.css'
 
 const customStyles = {
     overlay: {
-        backgroundColor: "transparent",
-        display: "flex",
-        justifyContent: "center"
+        backgroundColor: 'transparent',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
     },
     content: {
         width: '50%',
         height: '80vh',
-        left: "25%",
-        right: "25%",
-        top: "10%",
+        left: '25%',
+        right: '25%',
+        top: '10%',
         background: 'linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0))',
         backdropFilter: 'blur(50px)',
-        border: "2px solid rgb(92, 192, 246)"
-    }
+        border: '2px solid rgb(92, 192, 246)',
+    },
 };
+
 Modal.setAppElement('#root');
 
 export default function FlashCards() {
     const [flashCards, setFlashCards] = useState([]);
-    const [pending, setPending] = useState(true);
-    const [status, setStatus] = useState(false);
     const [createCard, setCreateCard] = useState(false);
     const [newCard, setNewCard] = useState({
         id: '',
         question: '',
         answer: '',
-        createdDate: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
+        createdDate: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes(),
         modifiedDate: '',
         difficultyLevel: 'easy',
-        category: 'General Knowledge'
+        category: 'General Knowledge',
     });
+    const [errors, setErrors] = useState({});
 
     useEffect(() => {
         fetchJsonData('http://localhost:3001/flashCards')
-            .then((response) => {
+            .then(response => {
                 setFlashCards(response);
-                setPending(false);
                 setNewCard({ ...newCard, id: (response.length + 1).toString() });
             });
-    }, [status]);
-    console.log(newCard)
+    }, []);
 
-    const validateForm = () => {
-        if (!newCard.question || !newCard.answer) {
-            alert("Question and Answer cannot be empty.");
-            return false;
+    const handleInputChange = (e) => {
+        const updatedCard = { ...newCard, [e.target.name]: e.target.value };
+        setNewCard(updatedCard);
+
+        let newErrors = {};
+        if (updatedCard.question === '') {
+            newErrors.question = 'Question is required';
+        } else if (flashCards.some(card => card.question === updatedCard.question)) {
+            newErrors.question = 'This question already exists';
         }
-        if (flashCards.some(card => card.question === newCard.question)) {
-            alert("This question already exists.");
-            return false;
+        if (updatedCard.answer === '') {
+            newErrors.answer = 'Answer is required';
         }
-        return true;
+        setErrors(newErrors);
     };
 
+    console.log(errors)
+    console.log(newCard)
+
     const handleCreateCard = async () => {
-        if (!validateForm()) return;
-    
+        if (Object.keys(errors).length > 0) return;
+        const updatedCard = { ...newCard, createdDate: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes() };
+        setNewCard(updatedCard);
+
         try {
             const response = await postJsonData('http://localhost:3001/flashCards', newCard);
-    
             if (response) {
-                console.log('Flash card created successfully!');
-                setNewCard(prevState => ({
-                    ...prevState,
+                setNewCard({
                     id: (flashCards.length + 1).toString(),
                     question: '',
                     answer: '',
-                    createdDate: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
+                    createdDate: new Date(Date.now()).getHours() + ':' + new Date(Date.now()).getMinutes(),
                     modifiedDate: '',
                     category: 'General Knowledge',
-                    difficultyLevel:'easy'
-                }));
-                setStatus(prev => !prev);
+                    difficultyLevel: 'easy',
+                });
+                setFlashCards([...flashCards, newCard]);
                 setCreateCard(false);
             } else {
                 console.error('Failed to create flash card.');
@@ -86,58 +91,64 @@ export default function FlashCards() {
             console.error('Error:', error);
         }
     };
-    
-    
 
     function openModal() {
         setCreateCard(true);
-        setNewCard(prevState => ({
-            ...prevState,
-            createdDate: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes(),
-        }));
     }
 
     function closeModal() {
         setCreateCard(false);
     }
 
-    
-
     return (
         <div>
-            <div className="CardsUpperPart">
+            <div className='CardsUpperPart'>
                 <button onClick={openModal}>Create Card</button>
             </div>
 
             <Modal isOpen={createCard} onRequestClose={closeModal} style={customStyles}>
-                <form action="" method="post">
-                    <input type="text" value={newCard.question} name="question" id="question" placeholder="Enter a valid question" onChange={(e) => { setNewCard({ ...newCard, question: e.target.value }) }} />
-                    <input type="text" value={newCard.answer} name="answer" id="answer" placeholder="Enter an answer" onChange={(e) => { setNewCard({ ...newCard, answer: e.target.value }) }} />
-                    <select name="difficultyLevel" id="difficultyLevel" onChange={(e) => { setNewCard({ ...newCard, difficultyLevel: e.target.value }) }}>
-                        <option value="easy">Easy</option>
-                        <option value="medium">Medium</option>
-                        <option value="hard">Hard</option>
-                    </select>
-                    <select onChange={(e) => setNewCard({ ...newCard, category: e.target.value })}>
-                        <option value="General Knowledge">General Knowledge</option>
-                        <option value="Mathematics">Mathematics</option>
-                        <option value="Programming">Programming</option>
-                        <option value="Programming">Programming</option>
-                        <option value="Technology">Technology</option>
-                        <option value="Literature">Literature</option>
-                        <option value="Geography">Geography</option>
-                        <option value="Language">Language</option>
-                        <option value="History">History</option>
-                        <option value="Science">Science</option>
-                        <option value="Sports">Sports</option>
-                        <option value="Movies">Movies</option>
-                        <option value="Music">Music</option>
-                        <option value="Art">Art</option>
-                    </select>
-                    <button type="button" onClick={handleCreateCard}>Create Flash Card</button>
+                <span className='spanOutsideofForm'>Create A New Flash Card</span>
+                <form action='' method='post' className='modalForm'>
+                    <div>
+                        <span>Please, enter an question for card: </span>
+                        <input type='text' value={newCard.question} name='question' placeholder='Enter a valid question' onChange={handleInputChange} />
+                        {errors.question && <div className='error'>{errors.question}</div>}
+                    </div>
+                    <div>
+                        <span>Please, enter relative answer to you question: </span>
+                        <input type='text' value={newCard.answer} name='answer' placeholder='Enter an answer' onChange={handleInputChange} />
+                        {errors.answer && <div className='error'>{errors.answer}</div>}
+                    </div>
+                    <div>
+                        <span>Please, select difficulty level of your question (easy as default)</span>
+                        <select name='difficultyLevel' onChange={handleInputChange} value={newCard.difficultyLevel}>
+                            <option value='easy'>Easy</option>
+                            <option value='medium'>Medium</option>
+                            <option value='hard'>Hard</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <span>Please, category of your question (General Knowledge as default)</span>
+                        <select name='category' onChange={handleInputChange} value={newCard.category}>
+                            <option value='General Knowledge'>General Knowledge</option>
+                            <option value='Mathematics'>Mathematics</option>
+                            <option value='Programming'>Programming</option>
+                            <option value='Technology'>Technology</option>
+                            <option value='Literature'>Literature</option>
+                            <option value='Geography'>Geography</option>
+                            <option value='Language'>Language</option>
+                            <option value='History'>History</option>
+                            <option value='Science'>Science</option>
+                            <option value='Sports'>Sports</option>
+                            <option value='Movies'>Movies</option>
+                            <option value='Music'>Music</option>
+                            <option value='Art'>Art</option>
+                        </select>
+                    </div>
+                    <button type='button' onClick={handleCreateCard} disabled={errors.question || errors.answer}>Create Flash Card</button>
                 </form>
             </Modal>
-
         </div>
     );
 }
