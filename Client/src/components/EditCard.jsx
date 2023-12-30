7362cd334bafa11fe8b8690b5bef8d14247bc54d
Gulaghaa <gulagaabdul123@gmail.com>
2023-12-30 23:58:56 +0400

    Edit Modal Updated

--- Client/src/components/EditCard.jsx
+++ Client/src/components/EditCard.jsx
@@ -1,10 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { updateJsonData, fetchJsonData } from '../services/getAPI';
 import '../style/EditCard.css';
 
 function EditCard({ id, initialQuestion, initialAnswer, setData, closePopup }) {
     const [question, setQuestion] = useState(initialQuestion);
     const [answer, setAnswer] = useState(initialAnswer);
+    const [isUpdated, setIsUpdated] = useState(false);
+    const [isQuestionUnique, setIsQuestionUnique] = useState(true);
+    const [canUpdate, setCanUpdate] = useState(false);
+
+
+
+
+    useEffect(() => {
+        const checkUniqueness = async () => {
+            const data = await fetchJsonData('http://localhost:3001/flashCards');
+            const isUnique = !data.some(card => card.question === question && card.id !== id);
+            setIsQuestionUnique(isUnique);
+        };
+
+        if (question !== initialQuestion || answer !== initialAnswer) {
+            setIsUpdated(true);
+            checkUniqueness();
+        } else {
+            setIsUpdated(false);
+        }
+    }, [question, answer, initialQuestion, initialAnswer, id]);
+
+    useEffect(() => {
+        setCanUpdate(isUpdated && isQuestionUnique && question.trim() !== '' && answer.trim() !== '');
+    }, [isUpdated, isQuestionUnique, question, answer]);
+
+
+    const stopPropagation = (e) => {
+        e.stopPropagation();
+    };
+
 
     const handleUpdate = async () => {
         try {
@@ -29,13 +60,14 @@ function EditCard({ id, initialQuestion, initialAnswer, setData, closePopup }) {
     };
 
     return (
-        <div className="editCardModal">
+        <div className="editCardModal" onClick={stopPropagation}>
             <div className="editCardModalContent">
                 <h3>Edit Card</h3>
                 <input type="text" value={question} onChange={(e) => setQuestion(e.target.value)} />
                 <input type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} />
-                <button onClick={handleUpdate}>Update</button>
+                <button onClick={handleUpdate} disabled={!canUpdate}>Update</button>
                 <button onClick={closePopup}>Close</button>
+                {!isQuestionUnique && <p>This question already exists.</p>}
             </div>
         </div>
     );
